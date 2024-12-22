5416af7be8b42cd1552285c3acffc15f3d0ef353
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-12-22 13:48:27 -0300

    feat: Add exercicios file

---
+++ exercicios/exercicio-01.dart
@@ -0,0 +1,16 @@
+/*
+Crie uma função que dado dois valores (passados como parâmetros) 
+mostre no console va soma, subtração, multiplicação e divisão desses valores.
+*/
+
+void calcularOperacoes(double valor1, double valor2) {
+
+  print('Soma: ${valor1 + valor2}');
+  print('Subtração: ${valor1 - valor2}');
+  print('Multiplicação: ${valor1 * valor2}');
+  print('Divisão: ${valor2 != 0 ? valor1 / valor2 : 'Não é possível dividir por zero.'}');
+}
+
+void main() {
+  calcularOperacoes(10, 2);
+}
